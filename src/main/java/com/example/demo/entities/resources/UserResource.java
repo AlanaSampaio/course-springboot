82fef624f7641a99675d0fa576acb001c911a988
Alana Sampaio <alana.sampaio2000@gmail.com>
2022-08-19 11:46:57 -0300

    JPA repository, dependency injection, database seeding

--- src/main/java/com/example/demo/entities/resources/UserResource.java
+++ src/main/java/com/example/demo/entities/resources/UserResource.java
@@ -1,5 +1,7 @@
 package com.example.demo.entities.resources;
 
+import javax.persistence.Table;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,12 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.entities.User;
 
 @RestController
-@RequestMapping(value = "/users")
+@RequestMapping(value = "/userss")
 public class UserResource {
 
 	@GetMapping
 	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "12345");
+		User u = new User(null, "Maria", "maria@gmail.com", "99999999", "12345");
 		return ResponseEntity.ok().body(u);
 	}
 }
